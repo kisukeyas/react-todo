a746bc922720f3308b5747c16a156019a086889a
Kisukeyas <kisuke.yas@gmail.com>
2022-10-03 00:25:59 +0900

    item component

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import './Todo.css';
 
 function Todo() {
-    return <p>Hello World</p>
+    const [item, setItem] = useState([
+        {key: 1, text: 'Learn JavaScript', done: false},
+        {key: 2, text: 'Learn JavaScript', done: false},
+        {key: 3, text: 'Learn JavaScript', done: false},
+    ])
+    return (
+        <div>
+            {item.map(item => (
+                <Item item={item}/>
+            ))}
+        </div>
+    )
 }
 
+function Input(params) {
+}
 
+function Nav(params) {
+}
+
+function Item({item}) {
+    return (
+        <label>
+            <input type="checkbox" name="" id="" />
+                {item.text} + {item.key}
+        </label>
+    )
+}
 
 
 export default Todo;
