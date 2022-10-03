1de54337a0a9397f04d714bb560343de70fa0452
Kisukeyas <kisuke.yas@gmail.com>
2022-10-04 00:52:52 +0900

    2022/10/4

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react';
 import './Todo.css';
+const getKey = () => Math.random().toString(32).substring(2);
 
 function Todo() {
     const [item, setItem] = useState([
-        {key: 1, text: 'Learn JavaScript', done: false},
-        {key: 2, text: 'Learn JavaScript', done: false},
-        {key: 3, text: 'Learn JavaScript', done: false},
+        {key: getKey(), text: 'Learn JavaScript', done: false},
+        {key: getKey(), text: 'Learn JavaScript', done: false},
+        {key: getKey(), text: 'Learn JavaScript', done: false},
     ])
+
+    const handleCheck = checked => {
+        const newItem = item.map(item => {
+            if (item.key === checked.key){
+                item.done = !item.done;
+            }
+            return item;
+        })
+        setItem(newItem);
+    }
+
     return (
         <div>
+            <Input item={item}/>
             {item.map(item => (
-                <Item item={item}/>
+                <Item item={item} checkBox={handleCheck} key={item.key}/>
             ))}
         </div>
     )
 }
 
-function Input(params) {
+function Input() {
+    const [name, setItem] = useState('taro');
+    const nameChange = (i) => setItem(i.target.value)
+    return(
+        <>
+            <p>Hello {name}</p>
+            <input type="text" onChange={nameChange}/>
+        </>
+    );
 }
 
 function Nav(params) {
 }
 
-function Item({item}) {
+function Item({item, checkBox}) {
+    const handleChange = () => {
+        checkBox(item);
+    };
     return (
-        <label>
-            <input type="checkbox" name="" id="" />
-                {item.text} + {item.key}
-        </label>
+        <div>
+            <input type="checkbox" name="" id="" onChange={handleChange}/>
+            {item.key} : {item.text}
+            {item.done ? <label>完了</label> : ""}
+        </div>
     )
 }
 
