54d765948a36d5dfe59b554c3a3c3d9ac1894e37
Kisukeyas <kisuke.yas@gmail.com>
2022-10-04 14:40:55 +0900

    2022/10/4 4

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -5,16 +5,6 @@ const getKey = () => Math.random().toString(32).substring(2);
 function Todo() {
     const [item, setItem] = useState([])
 
-    const handleCheck = checked => {
-        const newItem = item.map(item => {
-            if (item.key === checked.key){
-                item.done = !item.done;
-            }
-            return item;
-        })
-        setItem(newItem);
-    }
-
     const handleDelete = i => {
         setItem(item.filter((item,index) => (index !== i)));
     }
@@ -28,7 +18,7 @@ function Todo() {
             <h1>Todo App</h1>
             <Input onAdd={handleAdd}/>
             {item.map((item, index) => (
-                <Item item={item} index={index} checkedItem={handleCheck} key={item.key} deleteItem={handleDelete}/>
+                <Item item={item} index={index} deleteItem={handleDelete}/>
             ))}
         </div>
     )
@@ -49,10 +39,9 @@ function Input({onAdd}) {
     );
 }
 
-function Item({item, checkedItem, deleteItem, index}) {
-    const handleChange = () => {
-        checkedItem(item);
-    };
+function Item({item, deleteItem, index}) {
+    const [check, setCheck] = useState();
+    const handleChange = () => setCheck(item.done = !item.done);
 
     const handleDelete = () => {
         deleteItem(index);
@@ -62,7 +51,7 @@ function Item({item, checkedItem, deleteItem, index}) {
         <div className='task_component'>
             <input type="checkbox" name="" id="" onChange={handleChange}/>
             Task {index+1}:{item.text}
-            {item.done ? <label>Done</label> : ""}
+            {check ? <label>Done</label> : ""}
             <button type='submit' onClick={handleDelete}>X</button>
         </div>
     )
