6994c64185f3ab03848143e2460be8cdad140eeb
Kisukeyas <kisuke.yas@gmail.com>
2022-10-11 20:08:43 +0900

    完成（仮）

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import styles from './Todo.module.css';
-import { Button, Checkbox, IconButton } from '@mui/material';
+import {Button, Checkbox, IconButton } from '@mui/material';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useForm } from 'react-hook-form';
 
 
 const getKey = () => Math.random().toString(32).substring(2);
@@ -59,7 +60,7 @@ function Todo() {
             <h1>Todo App</h1>
             <Input onAdd={handleAdd}/>
             <div className={styles.item_list}>
-                <Filter changeFilter={handleFilter} value={filter}/>
+                <Filter changeFilter={handleFilter} filter={filter}/>
                 <ul>
                     {displayItem.map((item, index) => (
                         <Item item={item} index={index} deleteItem={handleDelete} checkChenge={handleChange} editText={handleEditText}/>
@@ -71,25 +72,31 @@ function Todo() {
 }
 
 function Input({onAdd}) {
-    const [text, setText] = useState('');
-    const textChange = (e) => setText(e.target.value);
-    const [date, setDate] = useState('');
-    const dateChange = (e) => setDate(e.target.value);
+    const { register, handleSubmit, getValues, setValue, formState:{errors}} = useForm();
+
     const submitItem = () => {
-        onAdd(text, date);
-        setText('');
-        setDate('');
+        onAdd(getValues('task'), getValues('date'));
+        setValue('task', '');
+        setValue('date', '');
+
     }
+
     return(
         <div className={styles.input_container}>
-            <IconButton className={styles.check_btn} type="submit" onClick={submitItem} color='primary'><AddCircleIcon /></IconButton>
-            <input type="text" onChange={textChange} value={text} className={styles.text_box} placeholder="Create new todo ..."/>
-            <input type="date" onChange={dateChange} value={date} className={styles.date_box}/>
+            <IconButton className={styles.check_btn} type="submit" onClick={handleSubmit(submitItem)} color='primary'><AddCircleIcon /></IconButton>
+            <div className={styles.input_box}>
+                <input type="text" placeholder="Create new todo ..." {...register('task', {required: 'fill in todo'})}/>
+                {errors.task ? <p className={styles.error_message}>{errors.task.message}</p>:'' }
+            </div>
+            <div className={styles.input_box}>
+                <input type="date" {...register('date', {required: 'fill in date'})}/>
+                {errors.date ? <p className={styles.error_message}>{errors.date.message}</p>:'' }
+            </div>
         </div>
     );
 }
 
-function Filter({changeFilter}) {
+function Filter({changeFilter, filter}) {
     const handleFilter = (key, e) => {
         e.preventDefault();
         changeFilter(key)
@@ -97,18 +104,15 @@ function Filter({changeFilter}) {
 
     return (
         <nav className={styles.filter_component}>
-            <a href="#" onClick={handleFilter.bind(null, 'ALL')}>All</a>
-            <a href="#" onClick={handleFilter.bind(null, 'TODO')}>ToDo</a>
-            <a href="#" onClick={handleFilter.bind(null, 'DONE')}>Done</a>
+            <Button variant={filter === 'ALL' ? 'contained' : 'text'} onClick={handleFilter.bind(null, 'ALL')}>All</Button>
+            <Button variant={filter === 'TODO' ? 'contained' : 'text'} onClick={handleFilter.bind(null, 'TODO')}>ToDo</Button>
+            <Button variant={filter === 'DONE' ? 'contained' : 'text'} onClick={handleFilter.bind(null, 'DONE')}>Done</Button>
         </nav>
     );
 }
 
 function Item({item, deleteItem, checkChenge, editText}) {
-    const [text, setText] = useState('');
-    const textChange = (i) => setText(i.target.value);
-    const [date, setDate] = useState('');
-    const dateChange = (e) => setDate(e.target.value);
+    const { register, handleSubmit, getValues, setValue, formState:{errors} } = useForm();
 
     const handleDelete = () => {
         deleteItem(item.id);
@@ -121,27 +125,32 @@ function Item({item, deleteItem, checkChenge, editText}) {
     const [isEdit, setIsEdit] = useState(false);
 
     const startEdit = () => {
-        setText(item.text);
-        setDate(item.date);
+        setValue('task',item.text);
+        setValue('date', item.date);
         setIsEdit(true);
     }
 
     const endEdit = () => {
-        editText(item.id, text, date);
+        editText(item.id, getValues('task'), getValues('date'));
         setIsEdit(false);
     }
 
     return (
         <li className={styles.item_container}>
             <Checkbox icon={<CheckCircleOutlineIcon />} checkedIcon={<CheckCircleIcon color='success'/>} className={styles.check_btn} onChange={handleChange} checked={item.done}/>
-            <div>
-                {isEdit? <input type="text" onChange={textChange} value={text} className={styles.text_box} placeholder="Create new todo ..."/>: item.text}
+            <div className={styles.box_edit}>
+                {isEdit? <input type="text" {...register('task', {required: 'fill in todo'})} placeholder="Edit your todo ..."/>: <p className={item.done? styles.sentence_done :''}>{item.text}</p> }
+                {errors.task ? <p className={styles.error_message}>{errors.task.message}</p>:'' }
             </div>
-            <div>
-                {isEdit? <input type="date" onChange={dateChange} value={date} className={styles.date_box}/>: item.date}
+            <div className={styles.box_edit}>
+                {isEdit? <input type="date" {...register('date', {required: 'fill in date'})}/>: <p className={item.done? styles.sentence_done :''}>{item.date}</p> }
+                {errors.date ? <p className={styles.error_message}>{errors.date.message}</p>:'' }
             </div>
-            {!isEdit?<IconButton type='submit' onClick={startEdit}><BorderColorOutlinedIcon /></IconButton>:<IconButton type='submit' onClick={endEdit}><BorderColorIcon color='success'/></IconButton>}
+            <div className={styles.edit_icon}>
+            {!isEdit?<IconButton type='submit' onClick={startEdit}><BorderColorOutlinedIcon /></IconButton>:<IconButton type='submit' onClick={handleSubmit(endEdit)}><BorderColorIcon color='success'/></IconButton>}
             <IconButton type='submit' onClick={handleDelete}><DeleteIcon /></IconButton>
+            </div>
+
         </li>
     )
 }
